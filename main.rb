a3eeed4af39b1cc55b362b908b88146ceba69065
Nathan Brown <nkabrown@gmail.com>
2014-09-30 12:44:55 -0400

    Email content and styling

--- main.rb
+++ main.rb
@@ -33,11 +33,10 @@ post '/contact' do
        :name=> name,  
      }  
    ],  
-   :html=> "<html><h1>Hi,</h1> <p>thank you for taking time to discover Japan’s beautiful spa’s</p> <p>We will be sending you a brochure to introduce you to all of the relaxing and rejuvenating choices that await you during your visit to Japan.</html>",  
+   :html=> "<html><h1>Hi " + name + ".</h1> <p style='font-size:20px;'>Thank you for taking the time to discover Japan’s beautiful spa’s.</p><p style='font-size:20px'>We will be sending you a brochure to introduce you to all of the relaxing and rejuvenating choices that await you during your visit to Japan.</p><img src='https://lh5.googleusercontent.com/RjowMrsP6VbfI5ztvxU3HzCJHiaCJJRMDC-R-PknQe8=w1902-h696-no' alt='waves'></html>",  
    :from_email=> "nkabrown@gmail.com",  
   }  
   sending = m.messages.send message  
   puts sending  
-
   redirect '/'
 end
